4b07c56a902fa81e317bf2411de4eeb698251c55
Linda Martinez <lindanmjw@gmail.com>
2021-10-07 14:12:15 -0400

    add moment date/time code

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.log("am i connected?")
+
+// Today's Date & Time
+var update = function() {
+    document.getElementById("currentDay")
+    .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+setInterval(update, 1000);
